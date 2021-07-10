f6b80c26f7573e4b46a1baa7dba237aba70ecb3d
iIIuminate <77865055+iIIuminate@users.noreply.github.com>
2021-07-09 18:30:43 -0700

    Login page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,64 @@
-import logo from './universalbadminton.jpg';
+import React from 'react';
+import ReactDOM from 'react-dom'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoginOpen: true,
+      isRegisterOpen: false
+    };
+  }
+
+  render() {
+    
+    return (
+      <div className="root-container">
+
+      </div>
+    );
+  }
 }
 
+class LoginBox extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  submitLogin(e) {}
+
+  render() {
+    return (
+      <div className="inner-container">
+        <div className="header">
+          Login
+        </div>
+        <div className="box">
+          <div className="input-group">
+            <input
+              type="text"
+              name="username"
+              className="login-input"
+              placeholder="Username"/>
+          </div>
+          <div className="input-group">
+            <input
+              type="password"
+              name="password"
+              className="login-input"
+              placeholder="Password"/>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render (
+  <App />, document.getElementById("root"));
 
 
-export default App;
+export default App
